e4a02079e455f22d968d3cf68858c3bde8a7811e
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-09-19 10:45:38 +0100

    initial commit

---
+++ model.py
@@ -0,0 +1,7 @@
+## Agent Practical 1 of 9 - Data Variables
+# Model:
+# Make a x variable.
+# Make a y variable.
+# Change x and y based on random numbers.
+# Make a second set of x and ys, and make these change randomly as well.
+# Work out the distance between two setes of x and ys.
