a7169d1020d2781a6552e7255e6abe5829a8a8c7
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-09-19 15:39:21 +0100

    End of practical 2

--- model.py
+++ model.py
@@ -3,57 +3,51 @@
 ## Imports:
 import random as rand
 import math
+import operator as op
+import matplotlib.pyplot as py
 
 agents = list()
 
 ## Model:
-# Make a x variable.
-x0 = 50
-
-# Make a y variable.
-y0 = 50
-
-# # Randomise start points instead
-# x0 = rand.randomint(0,100)
-# y0 = rand.randomint(0,100)
-
-agents.append([x0, y0])
+# # Make a x variable.
+# # Make a y variable.
+agents.append([rand.randint(0,100), rand.randint(0,100)])
 
 # Test that they've been assigned properly
-print('x0 = {0}, y0 = {1}'.format(x0, y0))
+print('x0 = {0}, y0 = {1}'.format(agents[0][0], agents[0][1]))
 
 # Change x and y based on random numbers.
 random_number = rand.random()
 if random_number < 0.5:
-	x0 += 1
+	agents[0][0] += 1
 else:
-	x0 -= 1
+	agents[0][0] -= 1
 
-print('random_number = {0}, therefore new x0 = {1}'.format(random_number, x0))
+print('random_number = {0}, therefore new x0 = {1}'.format(random_number, agents[0][0]))
 
 random_number = rand.random()
 if random_number < 0.5:
-	y0 += 1
+	agents[0][1] += 1
 else:
-	y0 -= 1
+	agents[0][1] -= 1
 
-print('random_number = {0}, therefore new y0 = {1}'.format(random_number, y0))
+print('random_number = {0}, therefore new y0 = {1}'.format(random_number, agents[0][1]))
 
 random_number = rand.random()
 if random_number < 0.5:
-	x0 += 1
+	agents[0][0] += 1
 else:
-	x0 -= 1
+	agents[0][0] -= 1
 
-print('random_number = {0}, therefore new x0 = {1}'.format(random_number, x0))
+print('random_number = {0}, therefore new x0 = {1}'.format(random_number, agents[0][0]))
 
 random_number = rand.random()
 if random_number < 0.5:
-	y0 += 1
+	agents[0][1] += 1
 else:
-	y0 -= 1
+	agents[0][1] -= 1
 
-print('random_number = {0}, therefore new y0 = {1}'.format(random_number, y0))
+print('random_number = {0}, therefore new y0 = {1}'.format(random_number, agents[0][1]))
 
 
 
@@ -61,53 +55,57 @@ print('random_number = {0}, therefore new y0 = {1}'.format(random_number, y0))
 # Make a second set of x and ys, and make these change randomly as well.
 
 # Make a x variable.
-x1 = 50
-
 # Make a y variable.
-y1 = 50
-
-# # Randomise start points instead
-# x1 = rand.randint(0,100)
-# y1 = rand.randint(0,100)
+agents.append([rand.randint(0,100), rand.randint(0,100)])
 
 # Test that they've been assigned properly
-print('x1 = {0}, y1 = {1}'.format(x1, y1))
+print('x1 = {0}, y1 = {1}'.format(agents[1][0], agents[1][1]))
 
 # Change x and y based on random numbers.
 random_number = rand.random()
 if random_number < 0.5:
-	x1 += 1
+	agents[1][0] += 1
 else:
-	x1 -= 1
+	agents[1][0] -= 1
 
-print('random_number = {0}, therefore new x1 = {1}'.format(random_number, x1))
+print('random_number = {0}, therefore new x1 = {1}'.format(random_number, agents[1][0]))
 
 random_number = rand.random()
 if random_number < 0.5:
-	y1 += 1
+	agents[1][1] += 1
 else:
-	y1 -= 1
+	agents[1][1] -= 1
 
-print('random_number = {0}, therefore new y1 = {1}'.format(random_number, y1))
+print('random_number = {0}, therefore new y1 = {1}'.format(random_number, agents[1][1]))
 
 random_number = rand.random()
 if random_number < 0.5:
-	x1 += 1
+	agents[1][0] += 1
 else:
-	x1 -= 1
+	agents[1][0] -= 1
 
-print('random_number = {0}, therefore new x1 = {1}'.format(random_number, x1))
+print('random_number = {0}, therefore new x1 = {1}'.format(random_number, agents[1][0]))
 
 random_number = rand.random()
 if random_number < 0.5:
-	y1 += 1
+	agents[1][1] += 1
 else:
-	y1 -= 1
+	agents[1][1] -= 1
 
-print('random_number = {0}, therefore new y1 = {1}'.format(random_number, y1))
+print('random_number = {0}, therefore new y1 = {1}'.format(random_number, agents[1][1]))
 
-print(x0, y0, x1, y1)
+print(agents)
 
 # Work out the distance between two setes of x and ys.
-answer = math.sqrt((x1-x0)**2 + (y1-y0)**2)
+answer = math.sqrt((agents[1][0]-agents[0][0])**2 + (agents[1][1]-agents[0][1])**2)
 print(answer)
+
+# Getting the most northern agent
+print(max(agents, key=op.itemgetter(1)))
+
+# A little bit of plotting
+py.xlim(0,99)
+py.ylim(0,99)
+py.scatter(agents[0][0], agents[0][1], color='red')
+py.scatter(agents[1][0], agents[1][1], color='green')
+py.show()
