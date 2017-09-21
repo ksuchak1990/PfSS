2e16b5159a71139c565ed732f4a869da2c3ef555
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-09-21 17:16:04 +0100

    End of practical 6

--- agentframework.py
+++ agentframework.py
@@ -1,39 +1,71 @@
 ## Agent Framework
 import random as rand
 class Agent():
-	# Constructor methods
-	def __init__(self):
-		self._x = rand.randint(0,100)
-		self._y = rand.randint(0,100)
+	## Constructor methods
+	def __init__(self, env):
+		self._environment = env
+		self._store = 0
+		self._env_width = len(env[0])
+		self._env_height = len(env)
+		self._x = rand.randint(0,self._env_width)
+		self._y = rand.randint(0,self._env_height)
 
-	# Accessor methods
+	## Accessor methods
+	# Access x
 	def getx(self):
 		return self._x
 
+	# Access y
 	def gety(self):
 		return self._y
+
+	# Access store
+	def getstore(self):
+		return self._store
 	
-	# Modifier methods
+	## Modifier methods
+	# Modify x
 	def setx(self, input_x):
 		self._x = input_x
 
+	# Modify y
 	def sety(self, input_y):
 		self._y = input_y
 
+	# Modify store
+	def setstore(self, input_store):
+		self._store = input_store
+
+	# Move
 	def move(self):
 		# Move x
 		if rand.random() < 0.5:
-			self._x = (self._x + 1) % 100
+			self._x = (self._x + 1) % self._env_width
 		else:
-			self._x = (self._x - 1) % 100
+			self._x = (self._x - 1) % self._env_width
 
 		# Move y
 		if rand.random() < 0.5:
-			self._y = (self._y + 1) % 100
+			self._y = (self._y + 1) % self._env_height
 		else:
-			self._y = (self._y - 1) % 100
+			self._y = (self._y - 1) % self._env_height
 
-	# Properties
-	x = property(fget=getx, fset=setx, doc="The x-coordinate of the agent")
-	y = property(fget=gety, fset=sety, doc="The y-coordinate of the agent")
+	# Eat
+	def eat(self):
+		if self._environment[self._y][self._x] > 10:
+			self._environment[self._y][self._x] -= 10
+			self._store += 10
 
+	# Sick
+	def sick(self):
+		if self._store > 1000:
+			self._environment[self._y][self._x] += self._store
+			self._store = 0
+
+	## Properties
+	x = property(fget=getx, fset=setx, doc='The x-coordinate of the agent')
+	y = property(fget=gety, fset=sety, doc='The y-coordinate of the agent')
+	store = property(fget=getstore, fset=setstore, doc='The store of the agent')
+
+	def __str__(self):
+		return 'x-coordinate = {0}, y-coordinate = {1}, store = {2}'.format(self._x, self._y, self._store)
--- model.py
+++ model.py
@@ -1,37 +1,63 @@
-## Agent Practical 5 of 9
+## Agent Practical 6 of 9
 
 ## Imports:
 import random as rand
 import math
 import operator as op
-import matplotlib.pyplot as py
+import matplotlib.pyplot as plt
 import agentframework
+import csv
 
-num_of_agents = 10
-num_of_iterations = 100
+## Constants:
+num_of_agents = 50
+num_of_iterations = 125
 agents = list()
 
+## Functions:
 def distance_between(agent0, agent1):
 	return math.sqrt((agent0.x - agent1.x)**2 + (agent0.y - agent1.y)**2)
 
+## Main:
+# Read in data
+environment = list()
+with open('in.txt', newline='') as f:
+	csvreader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+	for row in csvreader:
+		row_list = list()
+		for item in row:
+			row_list.append(int(item))
+		environment.append(row_list)
+
+#  Plot environment
+plt.xlim(0,len(environment[0]))
+plt.ylim(0,len(environment))
+plt.imshow(environment)
+plt.show()
+
 # Make agents
 for i in range(num_of_agents):
-	agents.append(agentframework.Agent())
+	agents.append(agentframework.Agent(environment))
 
 # Move agents
 for j in range(num_of_iterations):
-	for i in range(num_of_agents):
-		agents[i].move()
+	for agent in agents:
+		agent.move()
+		agent.eat()
+
+# Write out the end environment
+
+# Write agent stores
 
 # Plotting
-py.xlim(0,99)
-py.ylim(0,99)
-for i in range(num_of_agents):
-	py.scatter(agents[i].x, agents[i].y)
-py.show()
+plt.xlim(0,len(environment[0]))
+plt.ylim(0,len(environment))
+plt.imshow(environment)
+for agent in agents:
+	plt.scatter(agent.x, agent.y)
+plt.show()
 
-# Calculate distances
-for j in range(len(agents)):
-	for i in range(j+1, len(agents)):
-		distance = distance_between(agents[j], agents[i])
-		print('Distance between agent {0} and agent {1} = {2}'.format(j, i, distance))
+# # Calculate distances
+# for j in range(len(agents)):
+# 	for i in range(j+1, len(agents)):
+# 		distance = distance_between(agents[j], agents[i])
+# 		print('Distance between agent {0} and agent {1} = {2}'.format(j, i, distance))
