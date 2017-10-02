71b6245a27827251996899efeabe4f402dd8ceb7
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-10-02 15:57:39 +0100

    convert tabs to spaces, some further work for practical 6

--- agentframework.py
+++ agentframework.py
@@ -1,71 +1,97 @@
 ## Agent Framework
+
+## Imports
 import random as rand
+
+## Define Agent class
 class Agent():
-	## Constructor methods
-	def __init__(self, env):
-		self._environment = env
-		self._store = 0
-		self._env_width = len(env[0])
-		self._env_height = len(env)
-		self._x = rand.randint(0,self._env_width)
-		self._y = rand.randint(0,self._env_height)
+    """
+    Agent class:
+    A class to capture the behaviour of an abstract agent interacting with a given environment.
 
-	## Accessor methods
-	# Access x
-	def getx(self):
-		return self._x
+    Constructor takes argument:
+        env -- a list of lists characterising the 2-d landscape in which the agent exists (no default)
 
-	# Access y
-	def gety(self):
-		return self._y
+    Agent characteristics:
+        - store
+        - x coordinate
+        - y coordinate
 
-	# Access store
-	def getstore(self):
-		return self._store
-	
-	## Modifier methods
-	# Modify x
-	def setx(self, input_x):
-		self._x = input_x
+    Agent behaviours include:
+        - move
+        - eat
+        - sick
+    """
+    ## Constructor methods
+    def __init__(self, env):
+        self._environment = env
+        self._store = 0
+        self._env_width = len(env[0])   ## Assue that all rows are the same width
+        self._env_height = len(env)
+        self._x = rand.randint(0,self._env_width)
+        self._y = rand.randint(0,self._env_height)
 
-	# Modify y
-	def sety(self, input_y):
-		self._y = input_y
+    ## Accessor methods
+    # Access x
+    def getx(self):
+        return self._x
 
-	# Modify store
-	def setstore(self, input_store):
-		self._store = input_store
+    # Access y
+    def gety(self):
+        return self._y
 
-	# Move
-	def move(self):
-		# Move x
-		if rand.random() < 0.5:
-			self._x = (self._x + 1) % self._env_width
-		else:
-			self._x = (self._x - 1) % self._env_width
+    # Access store
+    def getstore(self):
+        return self._store
+    
+    ## Modifier methods
+    # Modify x
+    def setx(self, input_x):
+        self._x = input_x
 
-		# Move y
-		if rand.random() < 0.5:
-			self._y = (self._y + 1) % self._env_height
-		else:
-			self._y = (self._y - 1) % self._env_height
+    # Modify y
+    def sety(self, input_y):
+        self._y = input_y
 
-	# Eat
-	def eat(self):
-		if self._environment[self._y][self._x] > 10:
-			self._environment[self._y][self._x] -= 10
-			self._store += 10
+    # Modify store
+    def setstore(self, input_store):
+        self._store = input_store
 
-	# Sick
-	def sick(self):
-		if self._store > 1000:
-			self._environment[self._y][self._x] += self._store
-			self._store = 0
+    # Move
+    def move(self):
+        # Move x
+        if rand.random() < 0.5:
+            self._x = (self._x + 1) % self._env_width
+        else:
+            self._x = (self._x - 1) % self._env_width
 
-	## Properties
-	x = property(fget=getx, fset=setx, doc='The x-coordinate of the agent')
-	y = property(fget=gety, fset=sety, doc='The y-coordinate of the agent')
-	store = property(fget=getstore, fset=setstore, doc='The store of the agent')
+        # Move y
+        if rand.random() < 0.5:
+            self._y = (self._y + 1) % self._env_height
+        else:
+            self._y = (self._y - 1) % self._env_height
 
-	def __str__(self):
-		return 'x-coordinate = {0}, y-coordinate = {1}, store = {2}'.format(self._x, self._y, self._store)
+    # Eat
+    def eat(self):
+        if self._environment[self._y][self._x] > 10:
+            self._environment[self._y][self._x] -= 10
+            self._store += 10
+        # Eat less if there isn't much left (but obviously can't eat if there's 0 food)
+        elif self._environment[self._y][self._x] > 0:
+            self._environment[self._y][self._x] -= 1
+            self._store += 1
+
+    # Sick
+    def sick(self, quantity=50):
+        # Avoid using to high a quantity to throw up, as this can max out the matplotlib colour scale
+        if self._store > 1000:
+            self._environment[self._y][self._x] += quantity
+            self._store -= quantity
+
+    ## Properties
+    x = property(fget=getx, fset=setx, doc='The x-coordinate of the agent')
+    y = property(fget=gety, fset=sety, doc='The y-coordinate of the agent')
+    store = property(fget=getstore, fset=setstore, doc='The store of the agent')
+
+    def __str__(self):
+        return 'x-coordinate = {0}, y-coordinate = {1}, store = {2}'.format(self._x, self._y, self._store)
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 ## Agent Practical 6 of 9
+vis = True
 
 ## Imports:
 import random as rand
@@ -9,55 +10,62 @@ import agentframework
 import csv
 
 ## Constants:
-num_of_agents = 50
-num_of_iterations = 125
+num_of_agents = 10
+num_of_iterations = 10000
 agents = list()
 
 ## Functions:
 def distance_between(agent0, agent1):
-	return math.sqrt((agent0.x - agent1.x)**2 + (agent0.y - agent1.y)**2)
+    return math.sqrt((agent0.x - agent1.x)**2 + (agent0.y - agent1.y)**2)
 
 ## Main:
 # Read in data
 environment = list()
 with open('in.txt', newline='') as f:
-	csvreader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-	for row in csvreader:
-		row_list = list()
-		for item in row:
-			row_list.append(int(item))
-		environment.append(row_list)
+    csvreader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in csvreader:
+        row_list = list()
+        for item in row:
+            row_list.append(int(item))
+        environment.append(row_list)
 
 #  Plot environment
-plt.xlim(0,len(environment[0]))
-plt.ylim(0,len(environment))
-plt.imshow(environment)
-plt.show()
+if vis:
+    plt.xlim(0,len(environment[0]))
+    plt.ylim(0,len(environment))
+    plt.imshow(environment)
+    plt.show()
 
 # Make agents
 for i in range(num_of_agents):
-	agents.append(agentframework.Agent(environment))
+    agents.append(agentframework.Agent(environment))
 
 # Move agents
 for j in range(num_of_iterations):
-	for agent in agents:
-		agent.move()
-		agent.eat()
+    for agent in agents:
+        agent.move()
+        agent.eat()
+        agent.sick()
 
 # Write out the end environment
+with open('out.txt', 'w', newline='') as g:
+    csvwriter = csv.writer(g, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    for row in environment:
+        csvwriter.writerow(row)
 
 # Write agent stores
 
 # Plotting
-plt.xlim(0,len(environment[0]))
-plt.ylim(0,len(environment))
-plt.imshow(environment)
-for agent in agents:
-	plt.scatter(agent.x, agent.y)
-plt.show()
+if vis:
+    plt.xlim(0,len(environment[0]))
+    plt.ylim(0,len(environment))
+    plt.imshow(environment)
+    for agent in agents:
+        plt.scatter(agent.x, agent.y)
+    plt.show()
 
 # # Calculate distances
 # for j in range(len(agents)):
-# 	for i in range(j+1, len(agents)):
-# 		distance = distance_between(agents[j], agents[i])
-# 		print('Distance between agent {0} and agent {1} = {2}'.format(j, i, distance))
+#   for i in range(j+1, len(agents)):
+#       distance = distance_between(agents[j], agents[i])
+#       print('Distance between agent {0} and agent {1} = {2}'.format(j, i, distance))
