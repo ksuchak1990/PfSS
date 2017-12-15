2e50e218dea3c120870b6dddb8b776155fd2e599
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2017-12-15 11:08:01 +0000

    cleanup and make functions

--- model.py
+++ model.py
@@ -1,19 +1,21 @@
-## Agent Practical 7 of 9
-## Visualisations on/off
-vis = False
+# Agent Practical 7 of 9
 
 ## Imports:
-import random as rand
-import math
-import operator as op
-import matplotlib.pyplot as plt
-import agentframework
 import csv
 import sys
+import random as rand
+import matplotlib.pyplot as plt
+from agentframework import Agent
 
-## Constants:
+## Set up constants:
+### Visualisations and on/off
+vis = True
+### Command line args
 if len(sys.argv) != 4:
-    print('Incorrect number of args - reverting to default args')
+    if len(sys.argv) == 1:
+        print('No args provided - reverting to default args')
+    else:
+        print('Incorrect number of args - reverting to default args')
     num_of_agents = 10
     num_of_iterations = 100
     neighbourhood = 20
@@ -23,33 +25,51 @@ else:
     neighbourhood = int(sys.argv[3])
 print('Running for {0} agents, {1} iterations, neighbourhood = {2}'.format(num_of_agents, num_of_iterations, neighbourhood))
 
-agents = list()
+## Functions
+### Read in data
+def readEnvironment(fileName="in.txt"):
+    env = list()
+    with open(fileName, newline='') as f:
+        csvreader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        for row in csvreader:
+            row_list = list()
+            for item in row:
+                row_list.append(int(item))
+            env.append(row_list)
+    return env
+
+### Write out the final environment
+def writeEnvironment(data, fileName='out.txt'):
+    with open(fileName, 'w', newline='') as g:
+        csvwriter = csv.writer(g, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        for row in data:
+            csvwriter.writerow(row)
+
+def writeStores(data, fileName='stores.txt'):
+    # Write out final agent stores
+    stores = ','.join([str(agent.store) for agent in data])
+    with open('stores.txt', 'a') as h:
+        h.write('{0}\n'.format(stores))
 
 ## Main:
-# Read in data
-environment = list()
-with open('in.txt', newline='') as f:
-    csvreader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    for row in csvreader:
-        row_list = list()
-        for item in row:
-            row_list.append(int(item))
-        environment.append(row_list)
+### Initialisation
+agents = list()
+environment = readEnvironment()
 
-# Plot initial environment
+### Plot initial environment
 if vis:
     plt.xlim(0,len(environment[0]))
     plt.ylim(0,len(environment))
     plt.imshow(environment)
     plt.show()
 
-# Make agents
+### Make agents
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment, agents))
+    agents.append(Agent(environment, agents))
 
-# Move agents
+### Iterate agent interaction
 for j in range(num_of_iterations):
-    # Shuffle order of agents in each iteration
+    ### Shuffle order of agents in each iteration
     rand.shuffle(agents)
     for agent in agents:
         agent.move()
@@ -57,18 +77,10 @@ for j in range(num_of_iterations):
         # agent.sick()
         agent.share_with_neighbours(neighbourhood)
 
-# Write out the final environment
-with open('out.txt', 'w', newline='') as g:
-    csvwriter = csv.writer(g, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-    for row in environment:
-        csvwriter.writerow(row)
+writeEnvironment(data=environment)
+writeStores(data=agents)
 
-# Write out final agent stores
-with open('stores.txt', 'a') as h:
-    stores = ','.join([str(agent.store) for agent in agents])
-    h.write('{0}\n'.format(stores))
-
-# Plotting
+### Plotting
 if vis:
     plt.xlim(0,len(environment[0]))
     plt.ylim(0,len(environment))
